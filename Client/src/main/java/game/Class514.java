3dc3682e9da633a334a4dfa38307905ea4121882
revertus111 <revertus11@gmail.com>
2026-09-06 12:51:32 -0700

    Flush interface editor on client cycle

--- Client/src/main/java/game/Class514.java
+++ Client/src/main/java/game/Class514.java
@@ -52,6 +52,7 @@ public class Class514 {
 
 	static final void method6093(int i) {
 		CustomItemActionConfig.apply();
+		ClientConsoleInterfaceBridge.flushInterfaceEditorRequests();
 		ClientConsoleBridge.flushQueuedCommands();
 		if (-507155049 * client.anInt8790 != -1) {
 			int i_7_ = Class26.aClass564_216.method6657((short) -9926);
