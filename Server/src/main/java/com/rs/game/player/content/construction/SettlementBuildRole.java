a5e5cd1c195559568a2bf8a05169eddeae0d4387
revertus111 <revertus11@gmail.com>
2026-09-18 17:47:53 -0700

    Add settlement build roles

---
+++ Server/src/main/java/com/rs/game/player/content/construction/SettlementBuildRole.java
@@ -0,0 +1,12 @@
+package com.rs.game.player.content.construction;
+
+/**
+ * Stable semantic role for a settlement build piece.
+ *
+ * Art/object ids may change without changing milestone/build semantics.
+ */
+public enum SettlementBuildRole {
+    WALL,
+    FLOOR,
+    DOOR
+}
